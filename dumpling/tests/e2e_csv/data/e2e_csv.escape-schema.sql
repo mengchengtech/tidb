bc4a59e6a0ecd45409782058eaa3d6480faeddba
zhangb <zhangb@mctech.vip>
2022-06-23 15:20:00 +0800

    修改bug: 新加的mctech_sequence函数在select的结果中不管多少行数据只执行一次

--- dumpling/tests/e2e_csv/data/e2e_csv.escape-schema.sql
+++ dumpling/tests/e2e_csv/data/e2e_csv.escape-schema.sql
@@ -1,1 +1,4 @@
-../../naughty_strings/data/naughty_strings.escape-schema.sql
+CREATE TABLE `escape` (
+  `a` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  `b` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
