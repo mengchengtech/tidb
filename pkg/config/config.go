8aa3a1215975c5f36f01623dde64a8910a44a399
zhangb <zhangb@mctech.vip>
2022-07-06 01:10:02 +0800

    增加数据库联合查询规则检查和租户隔离的开关配置项

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1104,10 +1104,14 @@ var defaultConf = Config{
 			ApiPrefix: "http://node-infra-encryption-service.mc/",
 		},
 		DbChecker: DbChecker{
+			Enabled:          true,
 			ApiPrefix:        "http://node-infra-dim-service.mc/",
 			MutexAcrossDbs:   []string{},
 			ExcludeAcrossDbs: []string{},
 			AcrossDbGroups:   []string{},
+		},
+		Tenant: Tenant{
+			Enabled: true,
 		},
 	},
 }
