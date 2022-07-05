bc151c3aed53f72622df33ac209f9dc4a61d9c3b
zhangb <zhangb@mctech.vip>
2022-07-05 17:34:28 +0800

    除CRUD以外的其它代码不处理sql和验证租户信息

--- mctech/tenant/util.go
+++ mctech/tenant/util.go
@@ -6,32 +6,46 @@ import (
 )
 
 func ApplyTenantIsolation(context mctech.MCTechContext, node ast.Node,
-	charset string, collation string) (dbs []string, skipped bool, err error) {
-	switch node.(type) {
-	case *ast.UpdateStmt, *ast.DeleteStmt, *ast.SelectStmt, *ast.InsertStmt,
-		*ast.SetOprSelectList, *ast.SetOprStmt,
-		*ast.ExplainStmt:
-		v := NewTenantVisitor(context, charset, collation)
-		defer func() {
-			if e := recover(); e != nil {
-				err = e.(error)
-			}
-		}()
-		node.Accept(v)
+	charset, collation string) (dbs []string, skipped bool, err error) {
 
-		dbs = make([]string, 0, len(v.dbNames))
-		for n := range v.dbNames {
-			if n == "" {
-				n = context.CurrentDB()
-			}
-			dbs = append(dbs, n)
-		}
-		// var sb strings.Builder
-		// node.Restore(format.NewRestoreCtx(format.DefaultRestoreFlags|format.RestoreBracketAroundBinaryOperation, &sb))
-		// restoreSQL := sb.String()
-		// log.Info("<" + vars.CurrentDB + ">" + restoreSQL + "\r\n --> " + string(debug.Stack()))
-		return dbs, false, nil
+	var stmtNode ast.Node
+	if explainStmt, ok := node.(*ast.ExplainStmt); ok {
+		// ExplainStmt只需要处理对应的子句就可以
+		stmtNode = explainStmt.Stmt
+	} else {
+		stmtNode = node
+	}
+
+	switch stmtNode.(type) {
+	case *ast.UpdateStmt, *ast.DeleteStmt, *ast.SelectStmt, *ast.InsertStmt,
+		*ast.SetOprSelectList, *ast.SetOprStmt:
+		dbs, err = doApplyTenantIsolation(context, node, charset, collation)
+		return dbs, false, err
 	}
 
 	return nil, true, nil
+}
+
+func doApplyTenantIsolation(
+	context mctech.MCTechContext, node ast.Node, charset, collation string) (dbs []string, err error) {
+	v := NewTenantVisitor(context, charset, collation)
+	defer func() {
+		if e := recover(); e != nil {
+			err = e.(error)
+		}
+	}()
+	node.Accept(v)
+
+	dbs = make([]string, 0, len(v.dbNames))
+	for n := range v.dbNames {
+		if n == "" {
+			n = context.CurrentDB()
+		}
+		dbs = append(dbs, n)
+	}
+	// var sb strings.Builder
+	// node.Restore(format.NewRestoreCtx(format.DefaultRestoreFlags|format.RestoreBracketAroundBinaryOperation, &sb))
+	// restoreSQL := sb.String()
+	// log.Info("<" + vars.CurrentDB + ">" + restoreSQL + "\r\n --> " + string(debug.Stack()))
+	return dbs, err
 }
