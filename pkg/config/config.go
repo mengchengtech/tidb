d8998780170d237efc38808869ae1366fc49d273
zhangb <zhangb@mctech.vip>
2022-07-19 21:52:07 +0800

    禁用prepare命令

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1098,7 +1098,8 @@ var defaultConf = Config{
 			AcrossDbGroups:   []string{},
 		},
 		Tenant: Tenant{
-			Enabled: true,
+			Enabled:          true,
+			ForbiddenPrepare: true,
 		},
 		DDL: DDL{
 			Version: VersionColumn{
