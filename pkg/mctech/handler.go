5da60e07d9e89864bd87e07b3f17559e86004076
zhangb <zhangb@mctech.vip>
2022-07-19 21:35:21 +0800

    优化创建MCTechHandler的逻辑

--- pkg/mctech/handler.go
+++ pkg/mctech/handler.go
@@ -8,14 +8,15 @@ import (
 // Handler mctech enhance interface
 type Handler interface {
 	// PrapareSQL prapare sql
-	PrapareSQL() (sql string, err error)
+	PrapareSQL(session sessionctx.Context, rawSql string) (sql string, err error)
 	// ApplyAndCheck apply tenant isolation and check db policies
-	ApplyAndCheck(stmts []ast.StmtNode) (changed bool, err error)
+	ApplyAndCheck(session sessionctx.Context, stmts []ast.StmtNode) (changed bool, err error)
 }
 
 // HandlerFactory create Handler instance
 type HandlerFactory interface {
-	CreateHandler(session sessionctx.Context, sql string) Handler
+	CreateHandler() Handler
+	CreateHandlerWithContext(ctx Context) Handler
 }
 
 const handlerFactoryKey sessionValueKey = "$$MCTechContext"
