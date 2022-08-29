97d3931af4264858bcd3156beb22d726228159d3
zhangb <zhangb@mctech.vip>
2022-08-29 13:29:47 +0800

    use 语句支持 db名称改写

---
+++ mctech/msic/msic_extension_visitor_test.go
@@ -0,0 +1,106 @@
+package msic
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/pingcap/tidb/mctech"
+	"github.com/pingcap/tidb/parser"
+	. "github.com/pingcap/tidb/parser/format"
+	"github.com/stretchr/testify/require"
+)
+
+type testMCTechContext struct {
+	mctech.Context
+}
+
+type testDBSelector struct {
+	dbIndex mctech.DbIndex
+}
+
+func (s *testDBSelector) GetDbIndex() (mctech.DbIndex, error) {
+	return s.dbIndex, nil
+}
+
+type msicMCTechTestCase struct {
+	sql     string
+	expect  string
+	failure string
+}
+
+func (t msicMCTechTestCase) Source() any {
+	return t.sql
+}
+
+func (t *msicMCTechTestCase) Expect() string {
+	return t.expect
+}
+
+func (t *msicMCTechTestCase) Failure() string {
+	return t.failure
+}
+
+var (
+	useCases = []*msicMCTechTestCase{
+		{"use global_platform", "USE `mock_global_platform`", ""},
+	}
+)
+
+var msicExtensionCases = [][]*msicMCTechTestCase{
+	useCases,
+}
+
+func TestMsicExtensionVisitor(t *testing.T) {
+	for _, lst := range msicExtensionCases {
+		doRunTest(t, doRunMsicMCTechTestCase, lst)
+	}
+}
+
+func doRunMsicMCTechTestCase(t *testing.T, c *msicMCTechTestCase) error {
+	p := parser.New()
+	stmts, _, err := p.Parse(c.sql, "", "")
+	require.NoErrorf(t, err, "source %v", c.sql)
+	var sb strings.Builder
+	restoreSQLs := ""
+	for _, stmt := range stmts {
+		sb.Reset()
+		context, err := newTestMCTechContext()
+		if err != nil {
+			return err
+		}
+
+		if _, err := ApplyExtension(context, stmt); err != nil {
+			return err
+		}
+		err = stmt.Restore(NewRestoreCtx(DefaultRestoreFlags|RestoreBracketAroundBinaryOperation, &sb))
+		if err != nil {
+			return err
+		}
+
+		restoreSQL := sb.String()
+		if restoreSQLs != "" {
+			restoreSQLs += "; "
+		}
+		restoreSQLs += restoreSQL
+
+	}
+	require.Equalf(t, c.expect, restoreSQLs, "restore %v; expect %v", restoreSQLs, c.expect)
+	return nil
+}
+
+func newTestMCTechContext() (mctech.Context, error) {
+	result, err := mctech.NewPrepareResult("gslq4dev", map[string]any{
+		"dbPrefix": "mock",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	context := &testMCTechContext{
+		Context: mctech.NewBaseContext(
+			result, &testDBSelector{dbIndex: 1}),
+	}
+
+	return context, err
+}
---
+++ mctech/msic/util.go
@@ -0,0 +1,52 @@
+package msic
+
+import (
+	"sync"
+
+	"github.com/pingcap/tidb/mctech"
+	"github.com/pingcap/tidb/parser/ast"
+)
+
+type _msicExtension struct {
+}
+
+func (r *_msicExtension) Apply(context mctech.Context, node ast.Node) (matched bool, err error) {
+	matched = true
+	switch stmtNode := node.(type) {
+	case *ast.UseStmt:
+		err = r.changeToPhysicalDb(context, stmtNode)
+	default:
+		matched = false
+	}
+	return matched, err
+}
+
+func (r *_msicExtension) changeToPhysicalDb(context mctech.Context, n *ast.UseStmt) (err error) {
+	var dbName string
+	if dbName, err = context.ToPhysicalDbName(n.DBName); err == nil {
+		n.DBName = dbName
+	}
+
+	return err
+}
+
+var msicResolver *_msicExtension
+var msicResolverInitOne sync.Once
+
+func getMsicExtension() *_msicExtension {
+	if msicResolver != nil {
+		return msicResolver
+	}
+
+	msicResolverInitOne.Do(func() {
+		e := &_msicExtension{}
+		msicResolver = e
+	})
+	return msicResolver
+}
+
+// ApplyExtension apply msic
+func ApplyExtension(context mctech.Context, node ast.Node) (matched bool, err error) {
+	ext := getMsicExtension()
+	return ext.Apply(context, node)
+}
