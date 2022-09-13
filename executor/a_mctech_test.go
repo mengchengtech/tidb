3c8a3690ccdcb1a8887bd193388de057b373a4fb
zhangb <zhangb@mctech.vip>
2022-09-13 14:35:16 +0800

    完善测试用例

--- executor/a_mctech_test.go
+++ executor/a_mctech_test.go
@@ -80,31 +80,86 @@ func TestPrepare(t *testing.T) {
 	defer clean()
 	tk := initMock(t, store)
 
-	var createTableSQL = strings.Join([]string{
-		"create table demo (",
+	var createTableSQL0 = strings.Join([]string{
+		"create table org_relation (",
+		"tenant varchar(50),",
+		"org_id bigint,",
+		"child_org_id bigint,",
+		"primary key(tenant, org_id, child_org_id)",
+		")",
+	}, "\n")
+	var createTableSQL1 = strings.Join([]string{
+		"create table project (",
 		"id bigint,",
 		"tenant varchar(50),",
-		"b int,",
+		"org_id bigint,",
+		"construct_status varchar(50),",
+		"is_removed tinyint(1),",
 		"primary key(id, tenant)",
 		")",
 	}, "\n")
-	tk.MustExec(createTableSQL)
-	tk.MustExec("insert into demo (id, tenant, b) values (1, 'gslq', 1), (2, 'gslq', 2)")
-	tk.MustExec("insert into demo (id, tenant, b) values (5, 'ztsj', 5), (6, 'ztsj', 6)")
+	var createTableSQL2 = strings.Join([]string{
+		"create table organization (",
+		"id bigint,",
+		"tenant varchar(50),",
+		"org_type varchar(50),",
+		"ext_type varchar(50),",
+		"is_removed tinyint(1),",
+		"primary key(id, tenant)",
+		")",
+	}, "\n")
+	tk.MustExec(createTableSQL0)
+	tk.MustExec(createTableSQL1)
+	tk.MustExec(createTableSQL2)
+
+	tk.MustExec(`insert into org_relation
+(tenant, org_id, child_org_id)
+values
+('mctest', 466585195139584, 466585195139584)
+,('mctest', 466585369301504, 466585369301504)
+,('crec4', 1241041712902656, 1241041712902656)
+,('crec4', 1241042011738624, 1241042191774208)
+`)
+	tk.MustExec(`insert into project
+(tenant, id, org_id, construct_status, is_removed)
+values
+('mctest', 466585195139584, 466585195139584, 'finished', 0)
+,('mctest', 466585369301504, 466585369301504, 'none', 0)
+,('crec4', 1241041712902656, 1241041712902656, 'none', 0)
+,('crec4', 1241042011738624, 1241042011738624, 'none', 0)
+`)
+	tk.MustExec(`insert into organization
+(id, tenant, org_type, ext_type, is_removed)
+values
+(466585195139584, 'mctest', 'project', 'project', 0)
+,(466585369301504, 'mctest', 'project', 'project', 0)
+,(1241041712902656, 'crec4', 'project', 'project', 0)
+,(1241042011738624, 'crec4', 'project', 'project', 0)
+`)
 
 	session := tk.Session()
 	mctechCtx := mctech.NewContext(session, false)
 	mctech.SetContextForTest(session, mctechCtx)
-	tk.MustExec(`prepare st from "select * from demo a
-	join demo b on a.id = b.id
-	where a.id < ?"`)
+	tk.MustExec(`prepare st from "WITH orgs AS (
+		SELECT child_org_id FROM global_platform.org_relation WHERE org_id IS NOT NULL AND IFNULL(org_id, '') IN(
+			SELECT DISTINCT IFNULL(org_id, '') FROM global_platform.project WHERE org_id IS NOT NULL AND (
+is_removed=FALSE AND construct_status IN (?, ?, ?)
+			AND org_id IS NOT NULL AND IFNULL(org_id, '') IN(SELECT DISTINCT IFNULL(id, '') FROM global_platform.organization WHERE id IS NOT NULL AND (
+org_type = ? AND ext_type = ? AND is_removed = FALSE
+))
+)
+		)
+)SELECT * FROM orgs"`)
 
 	mctechCtx = mctech.NewContext(session, false)
 	mctech.SetContextForTest(session, mctechCtx)
-	tk.MustExec("set @p1 = 6")
+	tk.MustExec("SET @p1 = 'termination', @p2 = 'finished', @p3 = 'none', @p4 = 'project'")
 
 	mctechCtx = mctech.NewContext(session, false)
 	mctech.SetContextForTest(session, mctechCtx)
-	result := tk.MustQuery(`/*& tenant:gslq */ execute st using @p1`)
+	result := tk.MustQuery(
+		`/*& tenant:mctest */
+	EXECUTE st USING @p1, @p2, @p3, @p4, @p4
+	`)
 	print(result)
 }
