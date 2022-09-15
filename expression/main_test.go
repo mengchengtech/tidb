7324ef7f8581c2bffa34d46db70bdd055d84c30a
zhangb <zhangb@mctech.vip>
2022-09-15 23:03:24 +0800

    单元测试调试通过

--- expression/main_test.go
+++ expression/main_test.go
@@ -55,6 +55,10 @@ func TestMain(m *testing.M) {
 		goleak.IgnoreTopFunction("github.com/golang/glog.(*loggingT).flushDaemon"),
 		goleak.IgnoreTopFunction("go.etcd.io/etcd/client/pkg/v3/logutil.(*MergeLogger).outputLoop"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
+		// add by zhangbing
+		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
+		goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
+		// add end
 	}
 
 	callback := func(i int) int {
