ee72722bbdd0e22023d650b0d2bbb95f62313d29
zhangb <zhangb@mctech.vip>
2022-09-15 23:03:24 +0800

    单元测试调试通过

--- pkg/expression/main_test.go
+++ pkg/expression/main_test.go
@@ -50,6 +50,10 @@ func TestMain(m *testing.M) {
 		goleak.IgnoreTopFunction("github.com/lestrrat-go/httprc.runFetchWorker"),
 		goleak.IgnoreTopFunction("go.etcd.io/etcd/client/pkg/v3/logutil.(*MergeLogger).outputLoop"),
 		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
+		// add by zhangbing
+		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
+		goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
+		// add end
 	}
 
 	goleak.VerifyTestMain(m, opts...)
