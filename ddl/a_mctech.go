0e7ffe02d3414ba3b2a143a0200d8dfa90e71033
zhangb <zhangb@mctech.vip>
2022-12-22 15:26:49 +0800

    修改bug，设置了auto_increment的长整型默认值被改成了 MCTECH_SEQUENCE

--- ddl/a_mctech.go
+++ ddl/a_mctech.go
@@ -6,20 +6,19 @@ import (
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pingcap/tidb/table"
-	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/logutil"
 	"go.uber.org/zap"
 )
 
 func setMCTechSequenceDefaultValue(c *table.Column, hasDefaultValue bool, setOnUpdateNow bool) {
-	if hasDefaultValue {
+	if hasDefaultValue || mysql.HasAutoIncrementFlag(c.GetFlag()) {
 		return
 	}
 
 	// For mctech_sequence Col, if is not set default value or not set null, use current timestamp.
 	if c.GetType() == mysql.TypeLonglong && mysql.HasNotNullFlag(c.GetFlag()) {
 		if setOnUpdateNow {
-			if err := c.SetDefaultValue(types.ZeroDatetimeStr); err != nil {
+			if err := c.SetDefaultValue("0"); err != nil {
 				logutil.BgLogger().Error("set default value failed", zap.Error(err))
 			}
 		} else {
