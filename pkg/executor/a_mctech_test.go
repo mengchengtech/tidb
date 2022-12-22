48535d86504afc0a3a7f1a87e82fa1c089b54740
zhangb <zhangb@mctech.vip>
2022-12-22 15:26:49 +0800

    修改bug，设置了auto_increment的长整型默认值被改成了 MCTECH_SEQUENCE

--- pkg/executor/a_mctech_test.go
+++ pkg/executor/a_mctech_test.go
@@ -64,6 +64,27 @@ func TestForbiddenPrepare(t *testing.T) {
 	}
 }
 
+func TestIntegerAutoIncrement(t *testing.T) {
+	store := testkit.CreateMockStore(t)
+	tk := initMock(t, store)
+	// Check for warning in case we can't set the auto_increment to the desired value
+	tk.MustExec("create table t(a bigint primary key auto_increment)")
+	res := tk.MustQuery("SHOW COLUMNS FROM t")
+
+	lst := []string{}
+	for _, row := range res.Rows() {
+		lst = append(lst, fmt.Sprintf("%v", row))
+	}
+
+	require.Equal(t,
+		strings.Join([]string{
+			"[a bigint(20) NO PRI <nil> auto_increment]",
+		}, "\n"),
+		strings.Join(lst, "\n"),
+		"TestIntegerAutoIncrement",
+	)
+}
+
 func TestPrepareByQuery(t *testing.T) {
 	option := mctech.GetOption()
 	forbidden := option.ForbiddenPrepare
