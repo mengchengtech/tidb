3c5ea4c168c7cbcf142787dc709e8f5110789cc1
zhangbing <zhangb@mctech.vip>
2023-07-28 12:02:59 +0800

    修改附件的代码中部分方法的返回值结构，在不能生成上下文的情况下，根据是否测试环境决定是否返回异常信息

--- pkg/testkit/testkit.go
+++ pkg/testkit/testkit.go
@@ -347,10 +347,12 @@ func (tk *TestKit) ExecWithContext(ctx context.Context, sql string, args ...inte
 		prevWarns := sc.GetWarnings()
 		// add by zhangbing
 		handler := mctech.GetHandler()
-		mctechCtx := mctech.GetContext(ctx)
+		mctechCtx, err := mctech.GetContext(ctx)
+		if err != nil {
+			return nil, err
+		}
 
 		if mctechCtx != nil {
-			var err error
 			if sql, err = handler.PrepareSQL(mctechCtx, sql); err != nil {
 				return nil, err
 			}
