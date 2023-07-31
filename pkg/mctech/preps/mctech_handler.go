7536a8c9f3eec95d838b5b58ee003cef7c27e7ef
zhangbing <zhangb@mctech.vip>
2023-07-31 18:53:46 +0800

    mctechCtx重命名为mctx

--- pkg/mctech/preps/mctech_handler.go
+++ pkg/mctech/preps/mctech_handler.go
@@ -19,7 +19,7 @@ type mctechHandler struct {
 }
 
 // PrepareSQL prepare sql
-func (h *mctechHandler) PrepareSQL(mctechCtx mctech.Context, rawSQL string) (sql string, err error) {
+func (h *mctechHandler) PrepareSQL(mctx mctech.Context, rawSQL string) (sql string, err error) {
 	option := mctech.GetOption()
 	if !option.TenantEnabled {
 		// 禁用租户隔离
@@ -27,18 +27,18 @@ func (h *mctechHandler) PrepareSQL(mctechCtx mctech.Context, rawSQL string) (sql
 	}
 
 	var result *mctech.PrepareResult
-	sql, result, err = preprocessor.PrepareSQL(mctechCtx, rawSQL)
+	sql, result, err = preprocessor.PrepareSQL(mctx, rawSQL)
 	if err != nil {
 		return "", err
 	}
 
-	modifyCtx := mctechCtx.(mctech.BaseContextAware).BaseContext().(mctech.ModifyContext)
+	modifyCtx := mctx.(mctech.BaseContextAware).BaseContext().(mctech.ModifyContext)
 	modifyCtx.SetPrepareResult(result)
 	modifyCtx.SetDBSelector(newDBSelector(result))
 	// 改写session上的数据库
-	vars := mctechCtx.Session().GetSessionVars()
+	vars := mctx.Session().GetSessionVars()
 	currDb := vars.CurrentDB
-	currDb, err = mctechCtx.ToPhysicalDbName(currDb)
+	currDb, err = mctx.ToPhysicalDbName(currDb)
 	if err != nil {
 		return "", err
 	}
@@ -47,9 +47,9 @@ func (h *mctechHandler) PrepareSQL(mctechCtx mctech.Context, rawSQL string) (sql
 }
 
 // ApplyAndCheck apply tenant isolation and check db policies
-func (h *mctechHandler) ApplyAndCheck(mctechCtx mctech.Context, stmts []ast.StmtNode) (changed bool, err error) {
+func (h *mctechHandler) ApplyAndCheck(mctx mctech.Context, stmts []ast.StmtNode) (changed bool, err error) {
 	option := mctech.GetOption()
-	vars := mctechCtx.Session().GetSessionVars()
+	vars := mctx.Session().GetSessionVars()
 	charset, collation := vars.GetCharsetInfo()
 	preprocessor := preprocessor
 	for _, stmt := range stmts {
@@ -63,17 +63,17 @@ func (h *mctechHandler) ApplyAndCheck(mctechCtx mctech.Context, stmts []ast.Stmt
 		}
 
 		if !matched {
-			if matched, err = msic.ApplyExtension(mctechCtx, stmt); err != nil {
+			if matched, err = msic.ApplyExtension(mctx, stmt); err != nil {
 				return false, err
 			}
 		}
 
 		// ddl 与dml语句不必重复判断
 		if !matched && option.TenantEnabled {
-			modifyCtx := mctechCtx.(mctech.BaseContextAware).BaseContext().(mctech.ModifyContext)
+			modifyCtx := mctx.(mctech.BaseContextAware).BaseContext().(mctech.ModifyContext)
 			modifyCtx.Reset()
 			// 启用租户隔离，改写SQL，添加租户隔离信息
-			if dbs, skipped, err = preprocessor.ResolveStmt(mctechCtx, stmt, charset, collation); err != nil {
+			if dbs, skipped, err = preprocessor.ResolveStmt(mctx, stmt, charset, collation); err != nil {
 				return false, err
 			}
 		}
@@ -84,7 +84,7 @@ func (h *mctechHandler) ApplyAndCheck(mctechCtx mctech.Context, stmts []ast.Stmt
 
 		if option.TenantEnabled && option.DbCheckerEnabled && len(dbs) > 0 {
 			// 启用数据库联合查询规则检查
-			if err = getDatabaseChecker().Check(mctechCtx, dbs); err != nil {
+			if err = getDatabaseChecker().Check(mctx, dbs); err != nil {
 				return changed, err
 			}
 		}
@@ -95,7 +95,7 @@ func (h *mctechHandler) ApplyAndCheck(mctechCtx mctech.Context, stmts []ast.Stmt
 
 		if option.TenantEnabled {
 			// 启用租户隔离，改写SQL，检查租户隔离信息
-			err = preprocessor.Validate(mctechCtx)
+			err = preprocessor.Validate(mctx)
 			if err != nil {
 				return changed, err
 			}
