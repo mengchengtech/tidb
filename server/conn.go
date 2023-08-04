7e92a7f0685f7cd10ad9118efbcbfb5a12bad032
zhangbing <zhangb@mctech.vip>
2023-08-05 00:10:18 +0800

    afterHandleStmt 移除sql参数

--- server/conn.go
+++ server/conn.go
@@ -1931,7 +1931,7 @@ func (cc *clientConn) handleQuery(ctx context.Context, sql string) (err error) {
 			}
 		}
 		// add by zhangbing
-		cc.afterHandleStmt(ctx, stmt, sql, err)
+		cc.afterHandleStmt(ctx, stmt, err)
 		// add end
 	}
 
