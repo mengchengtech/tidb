c0feafaa61e5bb0e4aa1fcba290b55ef95fb1286
zhangbing <zhangb@mctech.vip>
2023-08-06 13:37:14 +0800

    测试环境go-cache替换为map

---
+++ mctech/preps/cache.go
@@ -0,0 +1,50 @@
+package preps
+
+import (
+	"time"
+
+	goCache "github.com/patrickmn/go-cache"
+	"github.com/pingcap/tidb/util/intest"
+)
+
+type MCTechCache interface {
+	Get(key string) (any, bool)
+	Set(key string, value any)
+}
+
+type simpleMapCache struct {
+	rawCache map[string]any
+}
+
+func (sc *simpleMapCache) Get(key string) (any, bool) {
+	result, ok := sc.rawCache[key]
+	return result, ok
+}
+
+func (sc *simpleMapCache) Set(key string, value any) {
+	sc.rawCache[key] = value
+}
+
+type goCacheCache struct {
+	rawCache *goCache.Cache
+}
+
+func (goc *goCacheCache) Get(key string) (any, bool) {
+	result, ok := goc.rawCache.Get(key)
+	return result, ok
+}
+
+func (goc *goCacheCache) Set(key string, value any) {
+	goc.rawCache.Set(key, value, goCache.DefaultExpiration)
+}
+
+func (goc *goCacheCache) Init(expiration time.Duration, cleanup time.Duration) {
+	goc.rawCache = goCache.New(expiration, cleanup)
+}
+
+func NewCache(expiration time.Duration, cleanup time.Duration) MCTechCache {
+	if intest.InTest {
+		return &simpleMapCache{rawCache: make(map[string]any)}
+	}
+	return &goCacheCache{rawCache: goCache.New(expiration, cleanup)}
+}
