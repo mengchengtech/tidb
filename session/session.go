a4957269dcddf97aea2cfabc82efcd70dc54d4a8
zhangbing <zhangb@mctech.vip>
2023-09-19 17:59:09 +0800

    修改sql记录bug: select类型的sql记录的执行过程信息有可能与实际结果不一致

--- session/session.go
+++ session/session.go
@@ -2423,6 +2423,9 @@ func runStmt(ctx context.Context, se *session, s sqlexec.Statement) (rs sqlexec.
 	rs, err = s.Exec(ctx)
 	se.updateTelemetryMetric(s.(*executor.ExecStmt))
 	sessVars.TxnCtx.StatementCount++
+	// add by zhangbing
+	se.SetValue(sessionctx.MCTechExecStmtVarKey, s.(*executor.ExecStmt))
+	// add end
 	if rs != nil {
 		if se.GetSessionVars().StmtCtx.IsExplainAnalyzeDML {
 			if !sessVars.InTxn() {
@@ -2440,9 +2443,6 @@ func runStmt(ctx context.Context, se *session, s sqlexec.Statement) (rs sqlexec.
 	}
 
 	err = finishStmt(ctx, se, err, s)
-	// add by zhangbing
-	se.SetValue(sessionctx.MCTechExecStmtVarKey, s.(*executor.ExecStmt))
-	// add end
 	if se.hasQuerySpecial() {
 		// The special query will be handled later in handleQuerySpecial,
 		// then should call the ExecStmt.FinishExecuteStmt to finish this statement.
