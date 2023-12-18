417c9fd5a591d718d3824a36182dc82e6c443534
zhangbing <zhangb@mctech.vip>
2023-12-18 12:31:10 +0800

    修改br代码bug，ignore-placement开关未生效

--- br/pkg/task/restore.go
+++ br/pkg/task/restore.go
@@ -527,13 +527,6 @@ func configureRestoreClient(ctx context.Context, client *restore.Client, cfg *Re
 		return errors.Trace(err)
 	}
 
-	// add by zhangbing
-	if cfg.IgnorePlacement {
-		// ignore all placement policy
-		client.SetSupportPolicy(false)
-	}
-	// add end
-
 	return nil
 }
 
@@ -1107,6 +1100,9 @@ func filterRestoreFiles(
 			newDBName = db.Info.Name.O + cfg.RestoreDBSuffix
 			db.Info.Name = model.NewCIStr(newDBName)
 		}
+		if cfg.IgnorePlacement {
+			db.Info.PlacementPolicyRef = nil
+		}
 		// add end
 		dbs = append(dbs, db)
 		for _, table := range db.Tables {
@@ -1121,6 +1117,9 @@ func filterRestoreFiles(
 			if len(cfg.RestoreTableSuffix) > 0 {
 				newTBName := table.Info.Name.O + cfg.RestoreTableSuffix
 				table.Info.Name = model.NewCIStr(newTBName)
+			}
+			if cfg.IgnorePlacement {
+				table.Info.PlacementPolicyRef = nil
 			}
 
 			if table.Info.TiFlashReplica != nil {
