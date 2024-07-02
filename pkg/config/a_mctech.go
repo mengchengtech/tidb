56567130ce3525286bed5eb8bf8ecfa480f3763e
zhangbing <zhangb@mctech.vip>
2024-07-02 12:45:54 +0800

    根据lint检查结果修改代码

--- pkg/config/a_mctech.go
+++ pkg/config/a_mctech.go
@@ -2,6 +2,7 @@
 
 package config
 
+// MCTech mctech custom config
 type MCTech struct {
 	Sequence   Sequence   `toml:"sequence" json:"sequence"`
 	Encryption Encryption `toml:"encryption" json:"encryption"`
@@ -9,30 +10,34 @@ type MCTech struct {
 	Tenant     Tenant     `toml:"tenant" json:"tenant"`
 }
 
+// Sequence mctech_sequence functions used
 type Sequence struct {
-	ApiPrefix     string `toml:"api-prefix" json:"api-prefix"`
+	APIPrefix     string `toml:"api-prefix" json:"api-prefix"`
 	Backend       int64  `toml:"backend" json:"backend"`
 	Mock          bool   `toml:"mock" json:"mock"`
 	Debug         bool   `toml:"debug" json:"debug"`
 	MaxFetchCount int64  `toml:"max-fetch-count" json:"max-fetch-count"`
 }
 
+// DbChecker db isolation check used
 type DbChecker struct {
 	Enabled          bool     `toml:"enabled" json:"enabled"`
-	ApiPrefix        string   `toml:"api-prefix" json:"api-prefix"`
+	APIPrefix        string   `toml:"api-prefix" json:"api-prefix"`
 	MutexAcrossDbs   []string `toml:"mutex" json:"mutex"`
 	ExcludeAcrossDbs []string `toml:"exclude" json:"exclude"`
 	AcrossDbGroups   []string `toml:"across" json:"across"`
 }
 
+// Tenant append tenant condition used
 type Tenant struct {
 	Enabled bool `toml:"enabled" json:"enabled"`
 }
 
+// Encryption custom crypto function used
 type Encryption struct {
 	Mock      bool   `toml:"mock" json:"mock"`
-	ApiPrefix string `toml:"api-prefix" json:"api-prefix"`
-	AccessId  string `toml:"access-id" json:"access-id"`
+	APIPrefix string `toml:"api-prefix" json:"api-prefix"`
+	AccessID  string `toml:"access-id" json:"access-id"`
 }
 
 func init() {
@@ -42,16 +47,16 @@ func init() {
 			Debug:         false,
 			MaxFetchCount: 1000,
 			Backend:       3,
-			ApiPrefix:     "http://node-infra-sequence-service.mc/",
+			APIPrefix:     "http://node-infra-sequence-service.mc/",
 		},
 		Encryption: Encryption{
 			Mock:      false,
-			AccessId:  "oJEKJh1wvqncJYASxp1Iiw",
-			ApiPrefix: "http://node-infra-encryption-service.mc/",
+			AccessID:  "oJEKJh1wvqncJYASxp1Iiw",
+			APIPrefix: "http://node-infra-encryption-service.mc/",
 		},
 		DbChecker: DbChecker{
 			Enabled:          true,
-			ApiPrefix:        "http://node-infra-dim-service.mc/",
+			APIPrefix:        "http://node-infra-dim-service.mc/",
 			MutexAcrossDbs:   []string{},
 			ExcludeAcrossDbs: []string{},
 			AcrossDbGroups:   []string{},
