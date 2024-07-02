737390084e16baeeb97fa2382fa679b82608b2c2
zhangbing <zhangb@mctech.vip>
2024-07-02 23:26:55 +0800

    use 语句支持 db名称改写

--- pkg/mctech/option.go
+++ pkg/mctech/option.go
@@ -12,29 +12,47 @@ import (
 
 // Option mctech option
 type Option struct {
-	SequenceMock          bool
-	SequenceDebug         bool
+	// sequence是否使用mock模式，不执行rpc调用，从本地返回固定的值
+	SequenceMock bool
+	// 是否开启sequence取值过程的调试模式，输出更多的日志
+	SequenceDebug bool
+	// 每次rpc调用获取sequence的最大个数
 	SequenceMaxFetchCount int64
-	SequenceBackend       int64
-	SequenceAPIPrefix     string
+	// 后台并行获取sequence的最大并发数
+	SequenceBackend int64
+	// sequence服务的调用地址前缀
+	SequenceAPIPrefix string
 
 	// encryption
-	EncryptionMock      bool
-	EncryptionAccessID  string
+	EncryptionMock     bool
+	EncryptionAccessID string
+	// encryption服务的调用地址前缀
 	EncryptionAPIPrefix string
 
-	TenantEnabled    bool
+	// 是否启用租户隔离
+	TenantEnabled bool
+	// 禁用Prepare/Execute语句
 	ForbiddenPrepare bool
 
-	DbCheckerEnabled          bool
-	DbCheckerAPIPrefix        string
-	DbCheckerMutexAcrossDbs   []string
+	// 是否开启同一sql语句中引用的数据库共存约束检查
+	DbCheckerEnabled bool
+	//
+	DbCheckerMutexAcrossDbs []string
+	// 被排除在约束检查外的数据库名称
 	DbCheckerExcludeAcrossDbs []string
 	DbCheckerAcrossDbGroups   []string
+	// 获取global_dw_*的当前索引的服务地址前缀
+	DbCheckerAPIPrefix string
 
+	// 是否开启 create table自动插入特定的version列的特性
 	DDLVersionColumnEnabled bool
-	DDLVersionColumnName    string
-	DDLVersionFilters       []string
+	// version列的名称
+	DDLVersionColumnName string
+	// 插入version的表需要满足的条件
+	DDLVersionFilters []string
+
+	// 是否支持dbPrefix hint
+	MsicDbPrefixEnabled bool
 }
 
 var mctechOpts *Option
