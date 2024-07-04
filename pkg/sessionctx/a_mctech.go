4bb9e4583a222be753946c45deb17678aae7d71e
zhangbing <zhangb@mctech.vip>
2024-07-04 14:56:37 +0800

    重定义sql相关的日志输出格式

---
+++ pkg/sessionctx/a_mctech.go
@@ -0,0 +1,16 @@
+// add by zhangbing
+
+package sessionctx
+
+import "strings"
+
+func ResolveSession(sctx Context) (db string, user string, client string) {
+	sessVars := sctx.GetSessionVars()
+	db = strings.ToLower(sessVars.CurrentDB)
+	if sessVars.User != nil {
+		user = sessVars.User.Username
+		client = sessVars.User.Hostname
+	}
+
+	return db, user, client
+}
