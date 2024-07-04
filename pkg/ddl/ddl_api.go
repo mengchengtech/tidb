320dd17d3185139c279d5e124170ed7e49b95bc0
zhangbing <zhangb@mctech.vip>
2024-07-04 11:27:07 +0800

    修改bug: 表启用tikv后alter语句添加__version后，从tikv里查询的默认值列是null

--- pkg/ddl/ddl_api.go
+++ pkg/ddl/ddl_api.go
@@ -1637,6 +1637,9 @@ func removeOnUpdateNowFlag(c *table.Column) {
 
 func processDefaultValue(c *table.Column, hasDefaultValue bool, setOnUpdateNow bool) {
 	setTimestampDefaultValue(c, hasDefaultValue, setOnUpdateNow)
+	// add by zhangbing
+	setMCTechSequenceDefaultValue(c, hasDefaultValue, setOnUpdateNow)
+	// add end
 
 	setYearDefaultValue(c, hasDefaultValue)
 
@@ -5655,12 +5658,21 @@ func ProcessModifyColumnOptions(ctx sessionctx.Context, col *table.Column, optio
 			return errors.Trace(dbterror.ErrUnsupportedModifyColumn.GenWithStack("can't change column constraint (UNIQUE KEY)"))
 		case ast.ColumnOptionOnUpdate:
 			// TODO: Support other time functions.
+			// add by zhangbing
+			if col.GetType() == mysql.TypeLonglong {
+				if !expression.IsValidMCTechSequenceExpr(opt.Expr, &col.FieldType) {
+					return dbterror.ErrInvalidOnUpdate.GenWithStackByArgs(col.Name)
+				}
+				goto finish // 为了减少对原代码行数过多的改动，此处用了goto
+			}
+			// add end
 			if !(col.GetType() == mysql.TypeTimestamp || col.GetType() == mysql.TypeDatetime) {
 				return dbterror.ErrInvalidOnUpdate.GenWithStackByArgs(col.Name)
 			}
 			if !expression.IsValidCurrentTimestampExpr(opt.Expr, &col.FieldType) {
 				return dbterror.ErrInvalidOnUpdate.GenWithStackByArgs(col.Name)
 			}
+		finish: // add by zhangbing
 			col.AddFlag(mysql.OnUpdateNowFlag)
 			setOnUpdateNow = true
 		case ast.ColumnOptionGenerated:
