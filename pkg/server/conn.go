56b5cdfcab2f50bee221ddd9576cd30ae2057f0e
zhangbing <zhangb@mctech.vip>
2024-07-04 14:26:05 +0800

    修改单元测试辅助方法 移除mctech.WithContext方法，改为mctech.WithNewContext 移除mctech.{Get/Set}ContextForTest方法 testkit类型增加MustExecWithContext和MustQueryWithContext方法

--- pkg/server/conn.go
+++ pkg/server/conn.go
@@ -67,7 +67,6 @@ import (
 	"github.com/pingcap/tidb/pkg/infoschema"
 	"github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tidb/pkg/mctech"
-	"github.com/pingcap/tidb/pkg/mctech/preps"
 	"github.com/pingcap/tidb/pkg/metrics"
 	"github.com/pingcap/tidb/pkg/parser"
 	"github.com/pingcap/tidb/pkg/parser/ast"
@@ -1700,8 +1699,7 @@ func (cc *clientConn) handleQuery(ctx context.Context, sql string) (err error) {
 	prevWarns := sc.GetWarnings()
 	// add by zhangbing
 	handler := mctech.GetHandler()
-	mctechCtx := preps.NewContext(cc.ctx.Session, false)
-	ctx = mctech.WithContext(ctx, mctechCtx)
+	ctx, mctechCtx := mctech.WithNewContext3(ctx, cc.ctx.Session, false)
 	if sql, err = handler.PrepareSQL(mctechCtx, sql); err != nil {
 		return err
 	}
