063f33974e7204f006d0ff5a4934750233de7e35
zhangbing <zhangb@mctech.vip>
2024-07-04 11:53:04 +0800

    修改bug: 表启用tiflash后alter语句添加__version后，tiflash同步schema出错

--- pkg/expression/a_mctech.go
+++ pkg/expression/a_mctech.go
@@ -285,7 +285,7 @@ func IsValidMCTechSequenceExpr(exprNode ast.ExprNode, fieldType *types.FieldType
 	return true
 }
 
-func getNextSequence() (int64, error) {
+func GetNextSequence() (int64, error) {
 	return udf.GetCache().Next()
 }
 
@@ -296,7 +296,7 @@ func GetBigIntValue(ctx BuildContext, v interface{}, tp byte, fsp int) (d types.
 	case string:
 		upperX := strings.ToUpper(x)
 		if upperX == strings.ToUpper(ast.MCTechSequence) {
-			if value, err = getNextSequence(); err != nil {
+			if value, err = GetNextSequence(); err != nil {
 				return d, err
 			}
 		} else {
