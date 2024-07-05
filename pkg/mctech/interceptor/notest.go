8e071bef60ee24769161c2c1a1fa0be96652ce05
zhangbing <zhangb@mctech.vip>
2024-07-05 14:56:32 +0800

    full trace 增加maxCop.procAddr, maxCop.procTime ,txId, times.process, maxAct, category 几个字段；调整full trace log 输出的json格式

--- pkg/mctech/interceptor/notest.go
+++ pkg/mctech/interceptor/notest.go
@@ -21,3 +21,9 @@ func renderTraceLog(_ sessionctx.Context, fields []zapcore.Field) {
 func GetFullQueryTraceLog(sctx sessionctx.Context) (map[string]any, error) {
 	return nil, errors.New("[GetFullQueryTraceLog] not allow invoke")
 }
+
+// EncodeForTest placeholder. not allow invoke
+func EncodeForTest(num uint64) string {
+	err := errors.New("[EncodeForTest] not allow invoke")
+	panic(err)
+}
