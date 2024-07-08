a3f664367579341b05492e6107729abaa2d29658
zhangbing <zhangb@mctech.vip>
2024-07-08 22:52:29 +0800

    修改bug
    alter .... add column...时，存在not null，但没有设置默认值时，会去调用mctech_sequence方法。此处不应该参考timestamp类型的处理过程

--- pkg/mctech/preps/db_index_test.go
+++ pkg/mctech/preps/db_index_test.go
@@ -3,13 +3,15 @@ package preps
 import (
 	"testing"
 
+	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/pkg/mctech"
+	"github.com/pingcap/tidb/pkg/mctech/mock"
 	"github.com/stretchr/testify/require"
 )
 
 type testContextCase struct {
 	tenant   string
-	response string
+	response map[string]any
 	expect   mctech.DbIndex
 	params   map[string]any
 	failure  string
@@ -25,24 +27,23 @@ func (c *testContextCase) Source() any {
 
 func TestDbSelectorGetDbIndex(t *testing.T) {
 	cases := []*testContextCase{
-		{"gslq", `{"current": ""}`, 1, map[string]any{"background": true}, "cannot unmarshal"},
-		{"gslq", `{"db": ""}`, 1, map[string]any{"requestId": "12345678"}, "cannot unmarshal"},
-		{"gslq", `{"current": 2}`, 2, map[string]any{}, ""},
-		{"gslq", `{"current": 2}`, 2, map[string]any{}, ""}, // 测试缓存中获取
-		{"gslq", `{"current": 2}`, 1, map[string]any{"background": true}, ""},
-		{"gslq", `{"db": 1}`, 1, map[string]any{"requestId": "12345678"}, ""},
-		{"gslq", `{"db": 1}`, 1, map[string]any{"requestId": "12345678"}, ""}, // 测试重复执行缓存
+		{"gslq", map[string]any{"DbIndex.CurrentDB": map[string]any{"current": ""}}, 1, map[string]any{"background": true}, "cannot unmarshal"},
+		{"gslq", map[string]any{"DbIndex.DBByRequest": map[string]any{"db": ""}}, 1, map[string]any{"requestId": "12345678"}, "cannot unmarshal"},
+		{"gslq", map[string]any{"DbIndex.CurrentDB": map[string]any{"current": 2}}, 2, map[string]any{}, ""},
+		{"gslq", map[string]any{"DbIndex.CurrentDB": map[string]any{"current": 2}}, 2, map[string]any{}, ""}, // 测试缓存中获取
+		{"gslq", map[string]any{"DbIndex.CurrentDB": map[string]any{"current": 2}}, 1, map[string]any{"background": true}, ""},
+		{"gslq", map[string]any{"DbIndex.DBByRequest": map[string]any{"db": 1}}, 1, map[string]any{"requestId": "12345678"}, ""},
+		{"gslq", map[string]any{"DbIndex.DBByRequest": map[string]any{"db": 1}}, 1, map[string]any{"requestId": "12345678"}, ""}, // 测试重复执行缓存
 	}
 
 	doRunTest(t, contextRunTestCase, cases)
 }
 
 func contextRunTestCase(t *testing.T, c *testContextCase) error {
-	var rpcClient = mctech.GetRPCClient()
-	mctech.SetRPCClientForTest(&mockClient{})
-	defer mctech.SetRPCClientForTest(rpcClient)
-	getDoFunc = createGetDoFunc(c.response)
-
+	failpoint.Enable("github.com/pingcap/tidb/pkg/mctech/MockMctechHttp",
+		mock.M(t, c.response),
+	)
+	defer failpoint.Disable("github.com/pingcap/tidb/pkg/mctech/MockMctechHttp")
 	result, err := mctech.NewPrepareResult(c.tenant, c.params)
 	if err != nil {
 		return err
