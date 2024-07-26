a66f89c337d6ac80b118fc00dcbfb1d8188e0dfd
zhangbing <zhangb@mctech.vip>
2024-07-27 00:19:01 +0800

    添加mctech语句单元测试

--- pkg/planner/core/a_mctech.go
+++ pkg/planner/core/a_mctech.go
@@ -27,7 +27,7 @@ import (
 	"github.com/pingcap/tidb/pkg/util/rowcodec"
 )
 
-func (b *PlanBuilder) buildMCTech(ctx context.Context, stmt *ast.MCTechStmt) (Plan, error) {
+func (b *PlanBuilder) buildMCTech(_ context.Context, stmt *ast.MCTechStmt) (Plan, error) {
 	p := &MCTech{
 		Format:   stmt.Format,
 		ExecStmt: stmt.Stmt,
@@ -59,6 +59,7 @@ var (
 var columnDefs = []*columnDef{
 	{"global", mysql.TypeLonglong, longlongSize},
 	{"excludes", mysql.TypeVarchar, 128},
+	{"comments", mysql.TypeVarchar, 512},
 	{"tenant", mysql.TypeVarchar, 50},
 	{"tenant_from", mysql.TypeVarchar, 10},
 	{"db", mysql.TypeVarchar, 50},
@@ -130,11 +131,12 @@ func (e *MCTech) mctechPlanInRowFormat() (err error) {
 	var (
 		global     = false
 		excludes   = []string{}
-		tenantCode string
+		comments   = "{}"
+		tenant     string
 		tenantFrom = "none"
 		params     = map[string]any{}
 		db         = e.SCtx().GetSessionVars().CurrentDB
-		index      = mctech.DbIndex(-1)
+		index      = mctech.DbIndexNone
 		restoreSQL = sb.String()
 	)
 
@@ -144,8 +146,9 @@ func (e *MCTech) mctechPlanInRowFormat() (err error) {
 			global = result.Global()
 			params = result.Params()
 			excludes = result.Excludes()
-			tenantCode = result.Tenant().Code()
-			if tenantCode != "" {
+			comments = result.Comments().String()
+			tenant = result.Tenant().Code()
+			if tenant != "" {
 				if result.Tenant().FromRole() {
 					tenantFrom = "role"
 				} else {
@@ -155,7 +158,7 @@ func (e *MCTech) mctechPlanInRowFormat() (err error) {
 		}
 		index, err = mctx.GetDbIndex()
 		if err != nil {
-			index = -1
+			index = mctech.DbIndexNone
 		}
 	}
 
@@ -167,7 +170,8 @@ func (e *MCTech) mctechPlanInRowFormat() (err error) {
 	var row = []*types.Datum{
 		createDatum(global),
 		createDatum(strings.Join(excludes, ",")),
-		createDatum(tenantCode),
+		createDatum(comments),
+		createDatum(tenant),
 		createDatum(tenantFrom),
 		createDatum(db),
 		createDatum(int(index)),
