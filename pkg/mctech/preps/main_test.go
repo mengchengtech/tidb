2bd9be4fa65747abd79797a7159a63850656d228
zhangbing <zhangb@mctech.vip>
2024-11-27 10:53:33 +0800

    消除golint 发现的代码问题

--- pkg/mctech/preps/main_test.go
+++ pkg/mctech/preps/main_test.go
@@ -28,7 +28,7 @@ type mctechTestCase interface {
 	Failure() string
 }
 
-func createSession(t *testing.T, tk *testkit.TestKit, user string, roles ...string) types.Session {
+func createSession(_ *testing.T, tk *testkit.TestKit, user string, roles ...string) types.Session {
 	session := tk.Session()
 	vars := session.GetSessionVars()
 	vars.User = &auth.UserIdentity{Username: user, Hostname: "%"}
