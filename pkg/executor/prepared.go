d169dd5116b8e9775980c4e9d173acd6180e49dd
zhangbing <zhangb@mctech.vip>
2024-12-31 18:39:42 +0800

    禁用prepare命令

--- pkg/executor/prepared.go
+++ pkg/executor/prepared.go
@@ -23,6 +23,7 @@ import (
 	"github.com/pingcap/tidb/pkg/executor/internal/exec"
 	"github.com/pingcap/tidb/pkg/expression"
 	"github.com/pingcap/tidb/pkg/infoschema"
+	"github.com/pingcap/tidb/pkg/mctech"
 	"github.com/pingcap/tidb/pkg/parser"
 	"github.com/pingcap/tidb/pkg/parser/ast"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
@@ -77,6 +78,12 @@ func NewPrepareExec(ctx sessionctx.Context, sqlTxt string) *PrepareExec {
 
 // Next implements the Executor Next interface.
 func (e *PrepareExec) Next(ctx context.Context, _ *chunk.Chunk) error {
+	// add by zhangbing
+	option := mctech.GetOption()
+	if option.ForbiddenPrepare {
+		return errors.New("[mctech] PREPARE not allowed")
+	}
+	// add end
 	vars := e.Ctx().GetSessionVars()
 	if e.ID != 0 {
 		// Must be the case when we retry a prepare.
