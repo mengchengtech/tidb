37f5273bff54bbc8a886927324b7dc5edb884483
zhangbing <zhangb@mctech.vip>
2024-12-31 18:28:28 +0800

    根据lint检查结果修改代码

--- pkg/server/conn.go
+++ pkg/server/conn.go
@@ -1765,8 +1765,8 @@ func (cc *clientConn) handleQuery(ctx context.Context, sql string) (err error) {
 	prevWarns := sc.GetWarnings()
 	// add by zhangbing
 	session := cc.ctx.Session
-	handler := prapared.CreateMctechHandler(session, sql)
-	if _, err = handler.PrapareSql(); err != nil {
+	handler := prapared.CreateMCTechHandler(session, sql)
+	if _, err = handler.PrapareSQL(); err != nil {
 		return err
 	}
 	// add end
@@ -1781,7 +1781,7 @@ func (cc *clientConn) handleQuery(ctx context.Context, sql string) (err error) {
 		return cc.writeOK(ctx)
 	}
 	// add by zhangbing
-	if _, err = handler.ResolveAndValidate(stmts); err != nil {
+	if _, err = handler.ApplyAndCheck(stmts); err != nil {
 		return err
 	}
 	// add end
