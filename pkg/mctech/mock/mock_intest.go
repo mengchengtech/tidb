ab228086b67b99f4440d751f8c6ffcc006e04162
zhangbing <zhangb@mctech.vip>
2024-12-31 19:02:07 +0800

    修改bug
    alter .... add column...时，存在not null，但没有设置默认值时，会去调用mctech_sequence方法。此处不应该参考timestamp类型的处理过程

--- pkg/mctech/mock/mock_intest.go
+++ pkg/mctech/mock/mock_intest.go
@@ -1,3 +1,5 @@
+// 测试环境编译当前代码，配合failpoint模块使用
+
 //go:build intest
 
 package mock
@@ -10,6 +12,10 @@ import (
 )
 
 func M(t require.TestingT, v any) string {
+	if s, ok := v.(string); ok {
+		return fmt.Sprintf("return(`%s`)", s)
+	}
+
 	bytes, err := json.Marshal(v)
 	require.NoError(t, err)
 	return fmt.Sprintf("return(`%s`)", string(bytes))
