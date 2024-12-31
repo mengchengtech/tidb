97cff472b8895d57a020cc6379b3aff421e66325
zhangbing <zhangb@mctech.vip>
2024-12-31 18:14:12 +0800

    运行期集成添加租户条件

--- pkg/server/conn.go
+++ pkg/server/conn.go
@@ -65,6 +65,7 @@ import (
 	"github.com/pingcap/tidb/pkg/extension"
 	"github.com/pingcap/tidb/pkg/infoschema"
 	"github.com/pingcap/tidb/pkg/kv"
+	"github.com/pingcap/tidb/pkg/mctech/tenant"
 	"github.com/pingcap/tidb/pkg/meta/model"
 	"github.com/pingcap/tidb/pkg/metrics"
 	"github.com/pingcap/tidb/pkg/parser"
@@ -1716,6 +1717,11 @@ func (cc *clientConn) handleQuery(ctx context.Context, sql string) (err error) {
 	if len(stmts) == 0 {
 		return cc.writeOK(ctx)
 	}
+	// add by zhangbing
+	for _, stmt := range stmts {
+		tenant.ApplyTenantIsolation(cc.ctx.TiDBContext, stmt)
+	}
+	// add end
 
 	warns := sc.GetWarnings()
 	parserWarns := warns[len(prevWarns):]
