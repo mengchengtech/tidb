71f41e7a66314280598694d04d37a7e67e0b6ad5
zhangbing <zhangb@mctech.vip>
2024-12-31 15:32:31 +0800

    支持自定义hint "cross"和"impersonate"，支持修改当前sql跨库访问，模拟tenant_only角色的特性

--- pkg/mctech/interceptor/interceptor_test.go
+++ pkg/mctech/interceptor/interceptor_test.go
@@ -53,7 +53,7 @@ func TestFullSQLLog(t *testing.T) {
 	require.NotNil(t, logData)
 
 	require.Equal(t, map[string]any{
-		"db": "global_ec3", "dbs": "global_ec3", "usr": "root", "tenant": "cscrc",
+		"db": "global_ec3", "dbs": "global_ec3", "usr": "root", "tenant": "cscrc", "across": "",
 		"conn": "1", "tp": "select",
 		"at":   now,
 		"time": map[string]any{"all": "3.315821ms", "parse": "176.943µs", "plan": "1.417613ms", "cop": "128ms", "ready": "2.315821ms", "send": "1ms"},
