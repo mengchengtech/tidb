65ee1ba27757c375e9d2b8d7c7b5b4fd38d02776
zhangbing <zhangb@mctech.vip>
2025-01-01 21:02:06 +0800

    mc_decrypt函数支持mask功能

--- pkg/expression/a_mctech_test.go
+++ pkg/expression/a_mctech_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/pingcap/failpoint"
+	mmock "github.com/pingcap/tidb/pkg/mctech/mock"
 	"github.com/pingcap/tidb/pkg/parser/ast"
 	"github.com/pingcap/tidb/pkg/parser/mysql"
 	"github.com/pingcap/tidb/pkg/types"
@@ -77,16 +78,106 @@ func TestMCTechEncrypt(t *testing.T) {
 	_, err = evalBuiltinFunc(f, ctx, chunk.Row{})
 	require.NoError(t, err)
 }
-
 func TestMCTechDecrypt(t *testing.T) {
 	ctx := createContext(t)
 	fc := funcs[ast.MCTechDecrypt]
+
 	f, err := fc.getFunction(mock.NewContext(),
 		datumsToConstants(types.MakeDatums("{crypto}a4UzL7Cnyyc+D/sK6U7GJA==")))
 	require.NoError(t, err)
 	resetStmtContext(ctx)
 	_, err = evalBuiltinFunc(f, ctx, chunk.Row{})
 	require.NoError(t, err)
+}
+
+type decryptCase struct {
+	cipher     string
+	maskFrom   any
+	maskLength any
+	maskChar   string
+	expected   any
+	errMsg     string
+}
+
+func (c *decryptCase) String() string {
+	return fmt.Sprintf("cipher:%s,maskFrom:%v,maskLength:%v", c.cipher, c.maskFrom, c.maskLength)
+}
+
+func TestMCTechDecryptMask(t *testing.T) {
+	failpoint.Enable("github.com/pingcap/tidb/pkg/mctech/MockMctechHttp",
+		mmock.M(t, map[string]any{
+			"Crypto.AES": map[string]any{"key": "W1gfHNQTARa7Uxt7wua8Aw==", "iv": "a9Z5R6YCjYx1QmoG5WF9BQ=="},
+		}),
+	)
+	failpoint.Enable("github.com/pingcap/tidb/pkg/mctech/udf/GetCryptoClient", mmock.M(t, "true"))
+	defer func() {
+		failpoint.Disable("github.com/pingcap/tidb/pkg/mctech/MockMctechHttp")
+		failpoint.Disable("github.com/pingcap/tidb/pkg/mctech/udf/GetCryptoClient")
+	}()
+
+	fc := funcs[ast.MCTechDecrypt]
+
+	ctx := createContext(t)
+	// "13511868785"
+	cipher := "{crypto}HMvlbGus4V3geqwFULvOUw=="
+	cases := []decryptCase{
+		{cipher, nil, 0, "", nil, ""},
+		{cipher, 0, nil, "", nil, ""},
+		{cipher, 0, 0, "", nil, "'maskFrom' (0) out of range [1, +inf]"},
+		{cipher, 0, 4, "", nil, "'maskFrom' (0) out of range [1, +inf]"},
+		{cipher, 1, 0, "", nil, "'maskLength' (0) out of range [1, 127]"},
+		{cipher, -1, 0, "", nil, "'maskFrom' (-1) out of range [1, +inf]"},
+		{cipher, 1, -1, "", nil, "'maskLength' (-1) out of range [1, 127]"},
+		{cipher, 1, 128, "", nil, "'maskLength' (128) out of range [1, 127]"},
+		{cipher, 1, 127, "", "***********", ""},
+		{cipher, 1, 11, "", "***********", ""},
+		{cipher, 1, 10, "", "**********5", ""},
+		{cipher, 1, 1, "", "*3511868785", ""},
+		{cipher, 11, 127, "", "1351186878*", ""},
+		{cipher, 12, 127, "", "13511868785", ""},
+		{cipher, 15, 127, "", "13511868785", ""},
+		{cipher, 4, 4, "", "135****8785", ""},
+		{cipher, 1, 4, "", "****1868785", ""},
+
+		{cipher, 1, 127, "@", "@@@@@@@@@@@", ""},
+		{cipher, 1, 11, "@", "@@@@@@@@@@@", ""},
+		{cipher, 1, 10, "@", "@@@@@@@@@@5", ""},
+		{cipher, 1, 1, "@", "@3511868785", ""},
+		{cipher, 11, 127, "@", "1351186878@", ""},
+		{cipher, 12, 127, "@", "13511868785", ""},
+		{cipher, 15, 127, "@", "13511868785", ""},
+		{cipher, 4, 4, "@", "135@@@@8785", ""},
+		{cipher, 1, 4, "@", "@@@@1868785", ""},
+
+		{cipher, 1, 127, "##", "######################", ""},
+		{cipher, 1, 11, "##", "######################", ""},
+		{cipher, 1, 10, "##", "####################5", ""},
+		{cipher, 1, 1, "##", "##3511868785", ""},
+		{cipher, 11, 127, "##", "1351186878##", ""},
+		{cipher, 12, 127, "##", "13511868785", ""},
+		{cipher, 15, 127, "##", "13511868785", ""},
+		{cipher, 4, 4, "##", "135########8785", ""},
+		{cipher, 1, 4, "##", "########1868785", ""},
+	}
+
+	for _, c := range cases {
+		args := []any{c.cipher, c.maskFrom, c.maskLength}
+		if len(c.maskChar) > 0 {
+			args = append(args, c.maskChar)
+		}
+		f, err := fc.getFunction(mock.NewContext(), datumsToConstants(types.MakeDatums(args...)))
+		require.NoError(t, err)
+		resetStmtContext(ctx)
+		result, err := evalBuiltinFunc(f, ctx, chunk.Row{})
+		if len(c.errMsg) > 0 {
+			require.Error(t, err, c.String())
+			require.Equal(t, c.errMsg, err.Error(), c.String())
+		} else {
+			require.NoError(t, err, c.String())
+			value := result.GetValue()
+			require.Equal(t, c.expected, value, c.String())
+		}
+	}
 }
 
 func TestGetFullSqlWithNotConfig(t *testing.T) {
