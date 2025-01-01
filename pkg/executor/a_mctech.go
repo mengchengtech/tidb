368159c9f6921e7b14f3c4c3d8241eb8d999f78f
zhangbing <zhangb@mctech.vip>
2025-01-01 21:27:25 +0800

    重写 prepare/execute 操作相关的注入逻辑

--- pkg/executor/a_mctech.go
+++ pkg/executor/a_mctech.go
@@ -41,6 +41,7 @@ import (
 	"github.com/pingcap/tidb/pkg/util/chunk"
 	"github.com/pingcap/tidb/pkg/util/execdetails"
 	"github.com/pingcap/tidb/pkg/util/hack"
+	"github.com/pingcap/tidb/pkg/util/intest"
 	"github.com/pingcap/tidb/pkg/util/logutil"
 	"github.com/pingcap/tidb/pkg/util/memory"
 	"go.uber.org/zap"
@@ -116,7 +117,7 @@ func (e *MCTechExec) Next(ctx context.Context, req *chunk.Chunk) error {
 	return nil
 }
 
-func (e *PrepareExec) beforePrepare(ctx context.Context) error {
+func (e *PrepareExec) checkPrepare(ctx context.Context) error {
 	if config.GetMCTechConfig().Tenant.ForbiddenPrepare {
 		return errors.New("[mctech] PREPARE not allowed")
 	}
@@ -126,9 +127,12 @@ func (e *PrepareExec) beforePrepare(ctx context.Context) error {
 func (e *PrepareExec) afterParseSQL(ctx context.Context, stmt ast.StmtNode) (err error) {
 	handler := mctech.GetHandler()
 	var mctx mctech.Context
-	mctx, err = mctech.GetContext(ctx)
-	if err != nil {
+	if mctx, err = mctech.GetContext(ctx); err != nil {
 		return err
+	}
+
+	if intest.InTest && mctx == nil {
+		return nil
 	}
 
 	if mctx != nil {
