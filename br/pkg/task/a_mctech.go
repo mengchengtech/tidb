f3af8530e1d490fbd91e1990ce83969c78b2fc6f
zhangb <zhangb@mctech.vip>
2025-02-19 16:51:32 +0800

    修改注入br的参数的命名方式

--- br/pkg/task/a_mctech.go
+++ br/pkg/task/a_mctech.go
@@ -2,19 +2,37 @@
 
 package task
 
+import "github.com/spf13/pflag"
+
 const (
-	// FlagSkipTiFlashRestore represents whether skip tiflash replica restore if tiflash exists
-	FlagSkipTiFlashRestore = "skip-tiflash-restore"
+	// flagSkipTiFlashRestore represents whether skip tiflash replica restore if tiflash exists
+	flagSkipTiFlashRestore = "mctech.skip-tiflash-restore"
 
-	// FlagForceReplicaCount represents set tiflash replica restore count if tiflash exists
-	FlagForceReplicaCount = "force-replica-count"
+	// flagForceReplicaCount represents set tiflash replica restore count if tiflash exists
+	flagForceReplicaCount = "mctech.force-replica-count"
 
-	// FlagIgnorePlacement represents whether ignore db and table placements
-	FlagIgnorePlacement = "ignore-placement"
+	// flagIgnorePlacement represents whether ignore db and table placements
+	flagIgnorePlacement = "mctech.ignore-placement"
 
-	// FlagRestoreTableSuffix represents restore table name suffix
-	FlagRestoreTableSuffix = "restore-table-suffix"
+	// flagRestoreTableSuffix represents restore table name suffix
+	flagRestoreTableSuffix = "mctech.restore-table-suffix"
 
-	// FlagRestoreDBSuffix represents restore database name suffix
-	FlagRestoreDBSuffix = "restore-db-suffix"
+	// flagRestoreDBSuffix represents restore database name suffix
+	flagRestoreDBSuffix = "mctech.restore-db-suffix"
 )
+
+type MCTechRestoreConfig struct {
+	SkipTiFlashRestore bool   `json:"skip-tiflash-restore" toml:"skip-tiflash-restore"`
+	ForceReplicaCount  uint16 `json:"force-replica-count" toml:"force-replica-count"`
+	IgnorePlacement    bool   `json:"ignore-placement" toml:"ignore-placement"`
+	RestoreTableSuffix string `json:"restore-table-suffix" toml:"restore-table-suffix"`
+	RestoreDBSuffix    string `json:"restore-db-suffix" toml:"restore-db-suffix"`
+}
+
+func defineMCTechRestoreFlags(flags *pflag.FlagSet) {
+	flags.Bool(flagSkipTiFlashRestore, false, "whether skip tiflash replica restore if tiflash exists (default false)")
+	flags.Uint16(flagForceReplicaCount, 0, "change tiflash replica count if tiflash exists, use restore meta information. no action when set 0 (default 0)")
+	flags.Bool(flagIgnorePlacement, false, "whether ignore db and table placements (default false)")
+	flags.String(flagRestoreTableSuffix, "", "restore table name suffix (default \"\")")
+	flags.String(flagRestoreDBSuffix, "", "restore db name suffix (default \"\")")
+}
