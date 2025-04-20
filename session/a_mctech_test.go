fa58e71f644bf41e6a823eb3386bec26c0d60009
zhangb <zhangb@mctech.vip>
2025-04-20 22:22:09 +0800

    digest_manager增加启用配置项

--- session/a_mctech_test.go
+++ session/a_mctech_test.go
@@ -14,6 +14,10 @@ import (
 )
 
 func TestCheckSQLDigest(t *testing.T) {
+	failpoint.Enable("github.com/pingcap/tidb/config/GetMCTechConfig",
+		mock.M(t, map[string]any{"SQLChecker.Enabled": true}),
+	)
+	defer failpoint.Disable("github.com/pingcap/tidb/config/GetMCTechConfig")
 	failpoint.Enable("github.com/pingcap/tidb/mctech/digestworker/GetDenyDigestInfo", mock.M(t, "digest-2"))
 	defer failpoint.Disable("github.com/pingcap/tidb/mctech/digestworker/GetDenyDigestInfo")
 
