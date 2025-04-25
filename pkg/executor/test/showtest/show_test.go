3410172a0ab6f0c552920470489ed2b296e8e360
zhangb <zhangb@mctech.vip>
2025-04-25 23:05:31 +0800

    添加打印自定义函数使用帮助的函数

--- pkg/executor/test/showtest/show_test.go
+++ pkg/executor/test/showtest/show_test.go
@@ -1413,7 +1413,7 @@ func TestShowBuiltin(t *testing.T) {
 	require.NotNil(t, res)
 	rows := res.Rows()
 	// modify by zhangbing
-	const builtinFuncNum = 305
+	const builtinFuncNum = 307
 	// modify end
 	require.Equal(t, builtinFuncNum, len(rows))
 	require.Equal(t, rows[0][0].(string), "abs")
