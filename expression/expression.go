939e8e9d8edc460159d1e5ea3a67473dbefb511f
zhangbing <zhangb@mctech.vip>
2026-03-26 11:55:03 +0800

    移除下推函数检查中关于扩展的函数的判断，原本默认都不下推

--- expression/expression.go
+++ expression/expression.go
@@ -1115,14 +1115,6 @@ func scalarExprSupportedByTiKV(sf *ScalarFunction) bool {
 			return false
 		}
 		return true
-		// add by zhangbing
-		// mctech functions
-	case
-		ast.MCSeq, ast.MCVersionJustPass, ast.MCDecrypt, ast.MCEncrypt, ast.MCSeqDecode, ast.MCGetFullSQL,
-		// 全名
-		ast.MCTechSequence, ast.MCTechVersionJustPass, ast.MCTechDecrypt, ast.MCTechEncrypt, ast.MCTechSequenceDecode, ast.MCTechGetFullSQL:
-		return false
-		// add end
 	}
 	return false
 }
@@ -1289,14 +1281,6 @@ func scalarExprSupportedByFlash(function *ScalarFunction) bool {
 		return true
 	case ast.IsIPv4, ast.IsIPv6:
 		return true
-		// add by zhangbing
-		// mctech functions
-	case
-		ast.MCSeq, ast.MCVersionJustPass, ast.MCDecrypt, ast.MCEncrypt, ast.MCSeqDecode, ast.MCGetFullSQL,
-		// 全名
-		ast.MCTechSequence, ast.MCTechVersionJustPass, ast.MCTechDecrypt, ast.MCTechEncrypt, ast.MCTechSequenceDecode, ast.MCTechGetFullSQL:
-		return false
-		// add end
 	}
 	return false
 }
